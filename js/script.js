e30d57d31d0882677ff99d5319857e6671f41b3e
Hanan-Hussein <hanhuss035@gmail.com>
2022-03-12 22:14:36 +0300

    ft:Description-Added toggle description on the what we do page

---
+++ js/script.js
@@ -0,0 +1,19 @@
+//toggling on and off the design
+$('#designIcon, #design').click(function(){
+    $('#designIcon').toggle();
+    $('#design').toggle();
+
+});
+//toggling off and on the development section
+$('#developmentIcon,#development').click(function(){
+    $('#development').toggle();
+    $('#developmentIcon').toggle();
+
+});
+//toggling off and on the product management section
+
+$('#products, #productsIcon').click(function(){
+    $('#products').toggle();
+    $('#productsIcon').toggle();
+})
+
